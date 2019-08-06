51da29e6837b5b60c7301be084c4b009f9e978ee
ysh <chineseyshes@163.com>
2019-08-06 09:17:17 +0800

    update HelloController

--- src/main/java/com/mystudy/springboot/HelloController.java
+++ src/main/java/com/mystudy/springboot/HelloController.java
@@ -1,6 +1,7 @@
 package com.mystudy.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +20,25 @@ public class HelloController {
 
     @GetMapping("/result/{name}")
     public Response result(@PathVariable("name") String name) {
+        // 根据币种名获取数据源ip
+        String localIp = config.getDataSource().get(name).get("localIp") + "";
+        String outerIp = config.getDataSource().get(name).get("outerIp") + "";
+
+        // 请求传递的参数
         Map<String, Object> params = new HashMap<>();
         params.put("name", name);
 
-//        String localIp = config.getDataSource().get(name).get("localIp");
-//        String outerIp = config.getDataSource().get(name).get("outerIp");
-//
-//        Response body = restTemplate.getForEntity(localIp + "/.../{name}", Response.class, params).getBody();
-//        Response body1 = restTemplate.getForEntity(outerIp + "/.../{name}", Response.class, params).getBody();
+        // 请求数据源获取数据
+        Response response1, response2;
+        if (StringUtils.isEmpty(localIp)) {
+            response1 = restTemplate.getForEntity(localIp + "/.../{name}", Response.class, params).getBody();
+        }
+        if (!StringUtils.isEmpty(outerIp)) {
+            response2 = restTemplate.getForEntity(outerIp + "/.../{name}", Response.class, params).getBody();
+        }
+
+        // 返回数据处理
+        // TODO
 
         return new Response();
     }
