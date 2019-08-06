2789b0521b29ceb83700f404ffff71a46069bad7
ysh <chineseyshes@163.com>
2019-08-06 08:33:03 +0800

    add yml factory update MyConfig

--- src/main/java/com/mystudy/springboot/MyConfig.java
+++ src/main/java/com/mystudy/springboot/MyConfig.java
@@ -6,19 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import java.util.List;
 import java.util.Map;
 
 @Component
-@PropertySource("classpath:/static/test.yml")
+@PropertySource(value = "classpath:/static/test.yml", factory = YamlPropertySourceFactory.class)
 @ConfigurationProperties(prefix = "currency")
 @Configuration
 public class MyConfig {
 
-    private List<Map<String, String>> dataSource;
+    private Map<String, Map<String, Object>> dataSource;
 
-    public List<Map<String, String>> getDataSource() {
+    public Map<String, Map<String, Object>> getDataSource() {
         return dataSource;
+    }
+
+    public void setDataSource(Map<String, Map<String, Object>> dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
